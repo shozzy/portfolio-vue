2a56d30b9932271b782deaef7112cb59292d6200
shozzy <shozzy.github@gmail.com>
2019-10-20 23:20:55 +0900

    ある程度コンポーネント化できた

--- pages/products.vue
+++ pages/products.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
-    {{ contents }}
+      <v-container fluid>
+        <v-row row wrap>
+          <v-col v-for="item in contents" v-bind:key="item.id" xs12 sm6 md4>
+            <ProductCard v-bind:title="item.title" v-bind:content="item.detail"/>
+          </v-col>
+        </v-row>
+      </v-container>
+<!--    <div v-for="item in contents">{{ item }}</div> -->
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-export default Vue.extend({
+import ProductCard from '~/components/ProductCard.vue'
+export default {
+  components: {
+    ProductCard
+  },
   data() {
     return {
-        contents: ''
+        contents: "",
     };
   },
   methods: {
-    async sendRequest() {
-      const response = await this.$axios.$get('/api/contents')
-      this.contents = response
+    async getJSON() {
+      try {
+        const response = await this.$axios.$get('/api/contents')
+        this.contents = response
+      } catch (e) {
+        console.error(e)
+      }
     }
   },
   mounted(){
-    this.sendRequest()
+    this.getJSON()
   }
-})
+}
 </script>

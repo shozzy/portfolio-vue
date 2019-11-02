f2d40bf81bf90c72e89566d1cdba57d23ecda51a
shozzy <shozzy.github@gmail.com>
2019-11-02 15:11:03 +0900

    ツールバーの初版ができた

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon>
         <img src='/shimaenaga_icon_small.png' class='icon' width='30px' alt='shimaenaga_icon'>
       </v-app-bar-nav-icon>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <!-- 幅900px以上くらいじゃないとはみ出す＝画面小さいときはメニュー表示形式変える -->
+      <v-toolbar-items v-for="item in pages">
+        <v-btn text nuxt :to="item.link">{{item.label}}</v-btn>
+      </v-toolbar-items>
+
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
@@ -29,7 +27,15 @@
 export default {
   data () {
     return {
-      title: "shozzy's portfolio"
+      title: "shozzy's portfolio",
+      pages: [
+        { label: "Top"     , link: "/"        },
+        { label: "Profile" , link: "/profile" },
+        { label: "Products", link: "/products"},
+        { label: "Skills"  , link: "/skills"  },
+        { label: "History" , link: "/history" },
+        { label: "Contact" , link: "/contact" },
+      ],
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,67 +9,12 @@
       sm8
       md6
     >
-      <v-card>
-        <v-card-title class="headline">
-          shozzy's portfolio 2
-        </v-card-title>
+      <v-card elevation="0">
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
+          <p>ようこそいらっしゃいませ。</p>
+          <p>ここはソフトウェアエンジニアである私"shozzy"のポートフォリオサイトです。</p>
+          <p>どうぞごゆっくりとご覧ください。</p>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>

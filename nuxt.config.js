4e7c2bc131ce993712a703f3b6d606d29f7e55a3
shozzy <shozzy.github@gmail.com>
2020-01-16 12:55:20 +0900

    一旦API決め打ちで確認する

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   },
   proxy: {
     '/api': {
-      target: process.env.API_BASE,
+      target: 'https://shozzy-portfolio-rails.herokuapp.com',
       pathRewrite: {
         '^/api': '/'
       }

6f79c189e2d11998c32fe24d78e09df2bfc0be5b
shozzy <shozzy.github@gmail.com>
2020-02-12 13:18:13 +0900

    contactページを更新

--- pages/contact.vue
+++ pages/contact.vue
@@ -11,56 +11,65 @@
     >
     <!-- 単なるカードじゃなくて、各サービスのアイコンを配置しよう -->
     <!-- 広い画面では横ならび、狭い画面では縦並びになるように調整 -->
+      <p>ご連絡はTwitterのDMか、下記お問い合わせフォームからお願いいたします。</p>
       <p>
-        <v-avatar size="32">
-          <a href="https://twitter.com/shozzy" target="_blank" rel="noopener">
-            <img src="/Twitter_Social_Icon_Circle_Color.png" class='icon' width='32px' alt="Twitter">
-          </a>
-        </v-avatar>
-        <v-avatar size="32">
-          <a href="https://qiita.com/shozzy" target="_blank" rel="noopener">
-            <img src="/qiita-favicon.png" class='icon' width='32px' alt="Qiita">
-          </a>
-        </v-avatar>
-        <v-avatar size="32">
-          <a href="https://github.com/shozzy" target="_blank" rel="noopener">
-            <img src="/GitHub-Mark-64px.png" class='icon' width='32px' alt="GitHub">
-          </a>
-        </v-avatar>
+        <v-btn href="https://twitter.com/shozzy" target="_blank" rel="noopener" color="white">
+          <v-avatar size="32" class="mr-2">
+              <img src="/Twitter_Social_Icon_Circle_Color.png" class='icon' width='32px' alt="Twitter">
+          </v-avatar>
+          Twitter
+        </v-btn>
+        <v-btn href="https://qiita.com/shozzy" target="_blank" rel="noopener" color="white">
+          <v-avatar size="32" class="mr-2">
+              <img src="/qiita-favicon.png" class='icon' width='32px' alt="Qiita">
+          </v-avatar>
+          Qiita
+        </v-btn>
+        <v-btn href="https://github.com/shozzy" target="_blank" rel="noopener" color="white">
+          <v-avatar size="32" class="mr-2">
+              <img src="/GitHub-Mark-64px.png" class='icon' width='32px' alt="GitHub">
+          </v-avatar>
+          GitHub
+        </v-btn>
       </p>
-      <p>以下のフォームからもお問いあわせいただけます。</p>
-      <p>
-      <v-form
-        ref="form"
-        width="80%"
-      >
-        <v-text-field
-          v-model="name"
-          label="お名前"
-          required
-        />
-        <v-text-field
-          v-model="email"
-          label="メールアドレス"
-          required
-        />
-        <v-text-field
-          v-model="company"
-          label="会社名（法人の方のみ）"
-        />
-        <v-textarea
-          v-model="message"
-          label="お問い合わせ内容"
-          required
-        />
-        <v-text-field
-          v-model="botfield"
-          label="人間は入力しないでください"
-          v-show="false"
-        />
-        <v-btn color="primary" @click="submit">送信</v-btn>
-      </v-form>
-      </p>
+      <v-card>
+        <v-card-title>お問いあわせフォーム</v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            width="80%"
+          >
+            <v-text-field
+              v-model="name"
+              label="お名前（必須）"
+              required
+              :rules="[varidations.required]"
+            />
+            <v-text-field
+              v-model="email"
+              label="メールアドレス（必須）"
+              required
+              :rules="[emailRules.required, emailRules.regex]"
+            />
+            <v-text-field
+              v-model="company"
+              label="会社名（法人の方のみ）"
+            />
+            <v-textarea
+              v-model="message"
+              label="お問い合わせ内容（必須）"
+              required
+              :rules="[varidations.required]"
+            />
+            <v-text-field
+              v-model="botfield"
+              label="人間は入力しないでください"
+              v-show="false"
+            />
+            <v-btn color="primary" @click="submit" :disabled="sendDisabled">{{sendLabel}}</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -74,6 +83,15 @@ export default {
       company: "",
       message: "",
       botfield: "",
+      sendLabel: "送信",
+      sendDisabled: false,
+      emailRules: {
+        required: v => !!v || 'メールアドレスを入力してください',
+        regex: v => /.+@.+\..+/.test(v) || '有効なメールアドレスを入力してください',
+      },
+      varidations: {
+        required: v => !!v || '入力必須です',
+      },
     }
   },
   mounted() {
@@ -84,24 +102,36 @@ export default {
   },
   methods: {
     async submit() {
-      const params = new FormData()
-      //以下、ダミーフォームの各フォーム要素のnameと合わせる
-      params.append('form-name', 'contact')
-      params.append('name', this.name)
-      params.append('email', this.email)
-      params.append('company', this.company)
-      params.append('message', this.message)
-      params.append('bot-field', this.botfield)
-      const response = await this.$axios.post(window.location.origin, params)
-        .catch(err => {
-          console.log('error.response='+err.response)
-        })
-      console.log('response='+response)
-      console.log('response.data='+response.data)
-      console.log('response.status='+response.status)
-      console.log('response.statusText='+response.statusText)
-      console.log('response.headers='+response.headers)
-      console.log('response.config='+response.config)
+      this.sendLabel = "送信中です..."
+      this.sendDisabled = true
+      if(this.$refs.form.validate()){
+        const params = new FormData()
+        //以下、ダミーフォームの各フォーム要素のnameと合わせる
+        params.append('form-name', 'contact')
+        params.append('name', this.name)
+        params.append('email', this.email)
+        params.append('company', this.company)
+        params.append('message', this.message)
+        params.append('bot-field', this.botfield)
+        const response = await this.$axios.post(window.location.origin, params)
+          .catch(err => {
+            console.log('error.response='+err.response)
+            this.sendLabel = "送信"
+            this.sendDisabled = false
+          })
+        //console.log('response='+response)
+        //console.log('response.data='+response.data)
+        console.log('response.status='+response.status)
+        console.log('response.statusText='+response.statusText)
+        //console.log('response.headers='+response.headers)
+        //console.log('response.config='+response.config)
+        if (response.status == "200"){
+          this.sendLabel = "送信が完了しました"
+        }
+      }else{
+        this.sendLabel = "送信"
+        this.sendDisabled = false
+      }
     },
   },
 }
@@ -115,5 +145,8 @@ img.icon {
   vertical-align: bottom;
   width: 32px;
   height: 32px;
+}
+.v-btn {
+  text-transform: none;
 }
 </style>

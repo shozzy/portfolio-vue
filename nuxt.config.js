2e3268f82a1137281208fa4dc7e3489de73f26f6
shozzy <shozzy.github@gmail.com>
2020-03-07 14:39:42 +0900

    title等を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,12 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s',
+    title: "shozzy's portfolio",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "ソフトウェアエンジニア'shozzy'のポートフォリオサイトです。" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
